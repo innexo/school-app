3d8846ddc0d0d853173b5902bf2e107b9477a195
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2019-08-03 21:12:35 -0700

    Learned Dart :walking:

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:web_socket_channel/io.dart';
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,67 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: appTitle,
-      home: MyHomePage(title: appTitle),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: Center(child: Text('My Page!')),
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            Container(
-              height: Theme.of(context).textTheme.body1.fontSize * 8,
-              child: 
-                DrawerHeader(
-                child: 
-                  
-                  Text(
-                    'Drawer Header',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                 ),
-              decoration: BoxDecoration(
-                color: Colors.blue[400],
-              ),
-             
-              margin: EdgeInsets.all(0.0),
-              padding: EdgeInsets.all(0.0)
-            ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
+      home: HomePage(
+        title: appTitle,
+        channel: IOWebSocketChannel.connect('ws://echo.websocket.org'),
         ),
-      ),
     );
   }
 }
---
+++ lib/post_display.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PostDisplay extends StatefulWidget {
+  
+  @override
+  _UpdateDisplay createState() => _UpdateDisplay();
+}
+  
+class _UpdateDisplay extends State<PostDisplay> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text("potato");
+  }
+}
