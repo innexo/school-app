3ef232fb80b280f98082bbf91fff54ead2e501de
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2019-08-06 02:32:20 -0700

    Drawer Header fixed

--- lib/navbars.dart
+++ lib/navbars.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'app_routes.dart';
 
@@ -39,6 +40,11 @@ class AppDrawer extends StatelessWidget {
             },
             selected: isSelected(context, Routes.campusMap),
           ),
+          _createDrawerItem(
+              text: 'Tutorial',
+              onTap: () {
+                _launchURL('https://flutter.dev');
+              }),
           Divider(),
           _createDrawerItem(
             text: 'Report an issue',
@@ -57,19 +63,25 @@ class AppDrawer extends StatelessWidget {
   }
 
   Widget _createHeader() {
-    return DrawerHeader(
-        margin: EdgeInsets.zero,
-        padding: EdgeInsets.zero,
-        child: Stack(children: <Widget>[
-          Positioned(
-              bottom: 12.0,
-              left: 16.0,
-              child: Text("Flutter Step-by-Step",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w500))),
-        ]));
+    return Container(
+      height: 80.0,
+      child: DrawerHeader(
+      margin: EdgeInsets.zero,
+      padding: EdgeInsets.zero,
+      child: Stack(children: <Widget>[
+        Positioned(
+          bottom: 12.0,
+          left: 16.0,
+          child: Text("Options",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.w500))),
+      ]),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+      ),
+    ));
   }
 
   Widget _createDrawerItem(
@@ -90,5 +102,13 @@ class AppDrawer extends StatelessWidget {
 
   bool isSelected(context, route) {
     return ModalRoute.of(context).settings.name.endsWith(route);
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
